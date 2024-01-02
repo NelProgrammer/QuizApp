c8520d318019146f760f5395643b0e3964fd4624
@NelProgrammer <nelsonlzweni@gmail.com>
2024-01-02 10:51:52 +0200

    Coded Quiz App as per the follow along. No new significant lesson here.
    Minor lesson was seeing how the solution to the type of problem is presented but this is nothing major.
    
    i.e from this nested array:
        {
            id: 'q1',
            text: 'Which of the following definitions best describes React.js?',
            answers: [
                'A library to build user interfaces with help of declarative code.',
                'A library for managing state in web applications.',
                'A framework to build user interfaces with help of imperative code.',
                'A library used for building mobile applications only.',
                ],
        },
    
    then Accessed as = array[index].title & array[index].nestedArray:
        <div id="question">
            <h2>{Dummy_Questions[activeQuestionIndex].text}</h2>
            <ul id="answers">
                {Dummy_Questions[activeQuestionIndex].answers.map((answer) => (
                <li key={answer} className="answer">
                    <button onClick={() => handleSelectAnswer(answer)}>
                    {answer}
                    </button>
                </li>
                ))}
            </ul>
        </div>

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import Header from './components/Header';
+import Quiz from './components/Quiz';
 
 const App = () => {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <main>
+        <Quiz />
+      </main>
+    </>
+  );
 };
 
 export default App;
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,8 +1,30 @@
+import { useState } from 'react';
+import Dummy_Questions from '../questions.js';
+
 const Quiz = () => {
+  const [userAnswers, setUserAnswers] = useState([]);
+  const activeQuestionIndex = userAnswers.length;
+
+  const handleSelectAnswer = (selectedAnswer) => {
+    setUserAnswers((prevUserAnswers) => {
+      return [...prevUserAnswers, selectedAnswer];
+    });
+  };
+
   return (
-    <div>
-      <img />
-      <h1>Actual Quiz</h1>
+    <div id="quiz">
+      <div id="question">
+        <h2>{Dummy_Questions[activeQuestionIndex].text}</h2>
+        <ul id="answers">
+          {Dummy_Questions[activeQuestionIndex].answers.map((answer) => (
+            <li key={answer} className="answer">
+              <button onClick={() => handleSelectAnswer(answer)}>
+                {answer}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
