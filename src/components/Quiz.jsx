00b48bdbdf8f6556312f3a5143eeb50360f39821
@NelProgrammer <nelsonlzweni@gmail.com>
2024-01-02 18:34:58 +0200

    Added the Quiz Completed page, to prevent breakage on the last quiz option.
    I have to say I dislike the patterns used by the tutor.
    One moment he was praizing useState, rightfully so.
    However, I am now seeing non state constants that look out of place.
    These are fed from changes in state.
    
    The up side is this has reinformed the idea that the entire component loads once state changes somethere.

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,18 +1,32 @@
 import { useState } from 'react';
 import Dummy_Questions from '../questions.js';
+import quizCompleteImg from '../assets/quiz-complete.png';
 
 const Quiz = () => {
   const [userAnswers, setUserAnswers] = useState([]);
   const activeQuestionIndex = userAnswers.length;
-
-  const shuffledAnswers = [...Dummy_Questions[activeQuestionIndex].answers];
-  shuffledAnswers.sort((a, b) => Math.random() - 0.5);
+  const quizIsComplete = activeQuestionIndex === Dummy_Questions.length;
 
   const handleSelectAnswer = (selectedAnswer) => {
     setUserAnswers((prevUserAnswers) => {
       return [...prevUserAnswers, selectedAnswer];
     });
   };
+
+  const quizScore = 50;
+
+  if (quizIsComplete) {
+    return (
+      <div id="summary">
+        <img src={quizCompleteImg} alt="Trophy icon"></img>
+        <h2>Quiz Completed!</h2>
+        <p>Your Score is ... {quizScore}</p>
+      </div>
+    );
+  }
+
+  const shuffledAnswers = [...Dummy_Questions[activeQuestionIndex].answers];
+  shuffledAnswers.sort((a, b) => Math.random() - 0.5);
 
   return (
     <div id="quiz">
